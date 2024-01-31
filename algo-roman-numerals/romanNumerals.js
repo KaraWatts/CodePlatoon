8fe6d2ad5a1f5a29efb98b01f0e179256bdf82cd
Kara Watts <kdwwatts@gmail.com>
2024-01-31 16:52:05 -0500

    solved roman numerals assignment

--- algo-roman-numerals/romanNumerals.js
+++ algo-roman-numerals/romanNumerals.js
@@ -1,9 +1,53 @@
 function toRomanLazy(num) {
-  return "";
+  const conversionObject = {
+    M : 1000,
+    D : 500,
+    C : 100,
+    L : 50,
+    X : 10,
+    V : 5,
+    I : 1
+  }
+  let result = ""
+  for (let key in conversionObject){
+    if (conversionObject[key] <= num){
+      const iteration = Math.floor(num/conversionObject[key]);
+      for (let i = 0; i < iteration; i++){
+        result += key
+      }
+      num -= conversionObject[key]*iteration
+    }
+  }
+  return result
 }
 
+
 function toRoman(num) {
-  return "";
+  const conversionObject = {
+    M : 1000,
+    CM : 900,
+    D : 500,
+    CD : 400,
+    C : 100,
+    L : 50,
+    XL : 40,
+    X : 10,
+    IX : 9,
+    V : 5,
+    IV : 4,
+    I : 1
+  }
+  let result = ""
+  for (let key in conversionObject){
+    if (conversionObject[key] <= num){
+      const iteration = Math.floor(num/conversionObject[key]);
+      for (let i = 0; i < iteration; i++){
+        result += key
+      }
+      num -= conversionObject[key]*iteration
+    }
+  }
+  return result
 }
 
 module.exports = { toRoman, toRomanLazy };
