da391292725c6923d1b9015edecaca0035cbcfc1
Kara Watts <kdwwatts@gmail.com>
2024-02-03 13:40:37 -0500

    made working armstrong test calculator without checker

--- algo-armstrong-numbers/js/armstrongNumbers.js
+++ algo-armstrong-numbers/js/armstrongNumbers.js
@@ -1,5 +1,16 @@
 // How can you make this more scalable and reusable later?
 
 exports.findArmstrongNumbers = function() {
+    numString = num.toString()
+    let n = numString.length
+    let sum = 0
+
+    for (let i = 0; i < n; i++){
+        digit = numString[i]
+        sum +=  parseInt(digit)**n
+    }
+
 
 };
+
+console.log(this.findArmstrongNumbers(371))
