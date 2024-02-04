ede2e043a7c8a7fe25cf747d1f002a14bc0dc6cc
Kara Watts <kdwwatts@gmail.com>
2024-02-04 18:38:23 -0500

    converted tests to pytest format and passed all tests

--- algo-credit-check/python/credit_check.py
+++ algo-credit-check/python/credit_check.py
@@ -6,13 +6,24 @@ def credit_check(cc_num):
 
 # reverse array
     num_list.reverse()
-    print(num_list)
+
 # 2x every other digit
-    double_every_other = [num*2 if index%2 == 0 else num for index, num in enumerate(num_list)]
-    print(double_every_other)
+    double_every_other = [num*2 if index%2 != 0 else num for index, num in enumerate(num_list)]
+    
+# sum digits of all products over 9
+    summed_products = [(int(str(num)[0]) + int(str(num)[1])) if num>9 else num for num in double_every_other]
+
+
 # sum all digits
+    total_sum = 0
+    for num in summed_products:
+        total_sum += num
 # sum % 10 == 0
-credit_check(5541808923795240)
+    validation = "The number is valid!" if total_sum % 10 == 0 else "The number is invalid!"
+    return validation
+
+
+
 
 
 # Your Luhn Algorithm Here
--- algo-credit-check/python/credit_check_spec.py
+++ algo-credit-check/python/credit_check_spec.py
@@ -1,11 +1,33 @@
-# Can you translate this driver code to unit tests?
+import pytest
 
 from credit_check import credit_check
 
-print(credit_check('5541808923795240') == "The number is valid!")
-print(credit_check("4024007136512380") == "The number is valid!")
-print(credit_check("6011797668867828") == "The number is valid!")
+def test_valid_string_input_one():
+    assert credit_check('5541808923795240') == "The number is valid!"
 
-print(credit_check("5541801923795240") == "The number is invalid!")
-print(credit_check("4024007106512380") == "The number is invalid!")
-print(credit_check("6011797668868728") == "The number is invalid!")
+def test_valid_string_input_two():
+    assert credit_check("4024007136512380") == "The number is valid!"
+
+def test_valid_string_input_three():
+    assert credit_check("6011797668867828") == "The number is valid!"
+
+def test_invalid_string_input_one():
+    assert credit_check("5541801923795240") == "The number is invalid!"
+
+def test_invalid_string_input_two():
+    assert credit_check("4024007106512380") == "The number is invalid!"
+    
+def test_invalid_string_input_three():
+    assert credit_check("6011797668868728") == "The number is invalid!"
+
+def test_valid_num_input():
+    assert credit_check(6011797668867828) == "The number is valid!"
+
+def test_invalid_num_input():
+    assert credit_check(5541801923795240) == "The number is invalid!"
+
+def test_too_few_numbers():
+    assert credit_check(6011867828) == "The number is invalid!"
+
+def test_too_many_numbers():
+    assert credit_check(554180192354354795240) == "The number is invalid!"
