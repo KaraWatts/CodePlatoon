a5850c56aaf8f555bcd549eb0aff7689d7b31640
Kara Watts <kdwwatts@gmail.com>
2024-02-16 21:17:09 -0500

    created recursive method to find smallest node and wrote additional tests, passed all tests

--- DataStructures/smallest-node/smallest_node.py
+++ DataStructures/smallest-node/smallest_node.py
@@ -5,7 +5,9 @@ class Node:
         self.left = left
         self.right = right
 
-
+    def __repr__(self):
+        return f"{self.value}"
+    
 class BinaryTree:
     def __init__(self):
         self.root = None
@@ -15,6 +17,9 @@ class BinaryTree:
             self.root = new_node
         else:
             self.insert_node(self.root, new_node)
+        
+    def __repr__(self):
+        return f"{self.root}"
 
     def insert_node(self, current_node, new_node):
         if new_node.value <= current_node.value:
@@ -29,3 +34,62 @@ class BinaryTree:
                 current_node.right = new_node
 
     # find_smallest goes here
+     # Recursively
+    def find_smallest(self, node = None):
+        if not node:
+            current = self.root
+        else:
+            current = node
+
+        if not current.left:
+            return current.value
+        
+        if not current.right or current.left.value < current.right.value:
+            return self.find_smallest(current.left)
+  
+
+
+# node1 = Node(5)
+# node2 = Node(4)
+# node3 = Node(10)
+# node4 = Node(2)
+# node5 = Node(7)
+# node6 = Node(6)
+# node7 = Node(8)
+# tree = BinaryTree()
+# tree.insert(node1)
+# tree.insert(node7)
+# tree.insert(node2)
+# tree.insert(node3)
+# tree.insert(node4)
+# tree.insert(node5)
+# tree.insert(node6)
+
+
+# # As a while loop   
+# def find_smallest_while(current):
+
+#     while current.left:
+#         if not current.right or current.left.value < current.right.value:
+#             current = current.left
+#         else:
+#             current = current.right
+
+#     return(current)
+
+
+# # Recursively
+# def find_smallest(current_node):
+#     try:
+#         current = current_node.root
+#     except:
+#         current = current_node
+#     if not current.left:
+#         return current
+    
+#     if not current.right or current.left.value < current.right.value:
+#         return find_smallest(current.left)
+#     else:
+#         return find_smallest(current.right)
+
+# print(find_smallest(tree))
--- DataStructures/smallest-node/tests.py
+++ DataStructures/smallest-node/tests.py
@@ -14,8 +14,57 @@ class TestSmallestNode(unittest.TestCase):
         self.tree.insert(self.node3)
         self.tree.insert(self.node4)
 
+    
+
     def test_find_smallest(self):
         self.assertEqual(self.tree.find_smallest(), 2)
+
+class TestSmallestNode2(unittest.TestCase):
+
+    def setUp(self):
+        self.node1 = Node(5)
+        self.node2 = Node(4)
+        self.node3 = Node(10)
+        self.node4 = Node(2)
+        self.node5 = Node(18)
+        self.node6 = Node(7)
+        self.node7 = Node(1)
+        self.tree = BinaryTree()
+        self.tree.insert(self.node1)
+        self.tree.insert(self.node2)
+        self.tree.insert(self.node3)
+        self.tree.insert(self.node4)
+        self.tree.insert(self.node5)
+        self.tree.insert(self.node6)
+        self.tree.insert(self.node7)
+
     
+    def test_find_smallest_2(self):
+        self.assertEqual(self.tree.find_smallest(), 1)
+
+class TestSmallestNode3(unittest.TestCase):
+    
+    def setUp(self):
+        self.node1 = Node(1)
+        self.node2 = Node(2)
+        self.node3 = Node(4)
+        self.node4 = Node(5)
+        self.node5 = Node(18)
+        self.node6 = Node(7)
+        self.node7 = Node(12)
+        self.tree = BinaryTree()
+        self.tree.insert(self.node1)
+        self.tree.insert(self.node2)
+        self.tree.insert(self.node3)
+        self.tree.insert(self.node4)
+        self.tree.insert(self.node5)
+        self.tree.insert(self.node6)
+        self.tree.insert(self.node7)
+
+    
+    def test_find_smallest_3(self):
+        self.assertEqual(self.tree.find_smallest(), 1)
+
+
 if __name__ == "__main__":
     unittest.main()
