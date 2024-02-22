b7253c8bb0cbc4898c6f6c3230c9ca1b770d6bb7
Kara Watts <kdwwatts@gmail.com>
2024-02-22 15:38:13 -0500

    created first sql scheme

--- FullStack/sql-basics/create_schema.sql
+++ FullStack/sql-basics/create_schema.sql
@@ -7,3 +7,27 @@ CREATE TABLE students (
   birthdate    date NOT NULL,
   address_id   integer
 );
+
+DROP TABLE IF EXISTS addresses;
+CREATE TABLE addresses (
+  id  serial PRIMARY KEY,
+  line_1 varchar(255) NOT NULL,
+  city varchar(255) NOT NULL,
+  state varchar(255) NOT NULL,
+  zipcode integer NOT NULL
+);
+
+DROP TABLE IF EXISTS classes;
+CREATE TABLE classes (
+  id serial PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  credits integer NOT NULL
+);
+
+DROP TABLE IF EXISTS enrollments;
+CREATE TABLE enrollments (
+  id serial PRIMARY KEY,
+  student_id integer NOT NULL,
+  class_id integer NOT NULL,
+  grade char(1)
+)
